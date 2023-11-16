aad2e52e8160b3c7f8900ebfa994e3e3490d7584
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2023-11-16 14:47:02 +0300

    add context "title" in main and catalog

--- store/products/views.py
+++ store/products/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        "title": "Главная страница UPGrade PC"
+    }
+    return render(request, 'products/index.html', context)
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        "title": "UPGrade PC - Каталог"
+    }
+    return render(request, 'products/products.html', context)
