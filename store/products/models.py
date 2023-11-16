535aad78438b27c9aad28b13f30eba2bc682b294
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2023-11-16 17:20:52 +0300

    create the first models for the database: ProductCategory, Product (создаем первые модели для базы данных: КатегорияТовара, Товар)

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+    is_available = models.BooleanField(default=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField()
+    specifications = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=0)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.PROTECT)
+    is_available = models.BooleanField(default=True)
