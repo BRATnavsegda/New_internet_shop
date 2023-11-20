bf3232b78d6c511bc7711d7a5666508ddb2207b6
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2023-11-20 23:33:49 +0300

    preparing the project for work with users

--- store/users/views.py
+++ store/users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def login(request):
+    context = {
+        "title": "Авторизация пользователя UPGrade PC"
+    }
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    context = {
+        "title": "Регистрация нового пользователя UPGrade PC"
+    }
+    return render(request, 'users/registration.html', context)
