a1866d4e7446c07630c5642e01deb6d8aafd9a69
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2023-11-21 15:39:48 +0300

    added profile page

--- store/users/views.py
+++ store/users/views.py
@@ -2,7 +2,7 @@ from django.contrib import auth
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 # Create your views here.
@@ -41,3 +41,18 @@ def registration(request):
         "form": form,
     }
     return render(request, 'users/registration.html', context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {
+        'title': 'Профиль пользователя UPGrade PC',
+        'form': form,
+    }
+    return render(request, 'users/profile.html', context)
