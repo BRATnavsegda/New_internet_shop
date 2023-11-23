d175b018c0ceca483430bac583a4983431a5b8e6
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2023-11-23 16:42:17 +0300

    change the index function view into index class view

--- store/products/views.py
+++ store/products/views.py
@@ -2,20 +2,22 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from products.models import Product, ProductCategory, Basket
 from django.core.paginator import Paginator
-from users.models import User
+from django.views.generic.base import TemplateView
 
 
 # Create your views here.
 
-def index(request):
-    context = {
-        "title": "Главная страница UPGrade PC"
-    }
-    return render(request, 'products/index.html', context)
+
+class IndexView(TemplateView):
+    template_name = 'products/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data()
+        context['title'] = "Главная страница UPGrade PC"
+        return context
 
 
 def products(request, category_id=None, page_number=1):
-
     # if category_id:
     #     products_ = Product.objects.filter(category__id=category_id)
     # else:
