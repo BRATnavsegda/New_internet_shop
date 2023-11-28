0e5cfa1ef96e696af1864b943ec50c71a49d98a5
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2023-11-29 01:22:48 +0300

    add tests for UserRegistrationView (all tests run: python manage.py test . )

--- store/users/tests.py
+++ store/users/tests.py
@@ -1,1 +1,51 @@
-# Create your tests here.
+from datetime import timedelta
+from http import HTTPStatus
+
+from django.test import TestCase
+from django.urls import reverse
+from django.utils.timezone import now
+
+from users.models import EmailVerification, User
+
+
+class UserRegistrationViewTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.path = reverse('users:registration')
+        self.test_user_data = {
+            'first_name': 'Petr', 'last_name': 'Petrov',
+            'username': 'PetrPetrov', 'email': 'petrpetrov@yandex.ru',
+            'password1': '12345678pP', 'password2': '12345678pP',
+        }
+        self.username_test_user = self.test_user_data['username']
+
+    def test_user_registration_get(self):
+        response = self.client.get(self.path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], "UPGrade PC - Регистрация")
+        self.assertTemplateUsed(response, 'users/registration.html')
+
+    def test_user_registration_post_success(self):
+        self.assertFalse(User.objects.filter(username=self.username_test_user).exists())
+        response = self.client.post(self.path, self.test_user_data)
+
+        #  check creating of user
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('users:login'), fetch_redirect_response=False)
+        self.assertTrue(User.objects.filter(username=self.username_test_user).exists())
+
+        #  check creating of email verification
+        email_verification = EmailVerification.objects.filter(user__username=self.username_test_user)
+        self.assertTrue(email_verification.exists())
+        self.assertEqual(
+            email_verification.first().expiration.date(),
+            (now() + timedelta(hours=48)).date()
+        )
+
+    def test_user_registration_post_error(self):
+        User.objects.create(username=self.username_test_user)
+        response = self.client.post(self.path, self.test_user_data)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, 'Пользователь с таким именем уже существует.', html=True)
