442d5eb16cee085fa33b006fc48e50e301cfd59d
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2023-12-20 18:39:43 +0300

    work with orders

--- store/orders/views.py
+++ store/orders/views.py
@@ -1,5 +1,7 @@
 from django.urls import reverse_lazy
+from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
+from django.shortcuts import HttpResponseRedirect
 
 from orders.forms import OrderForm
 from common.views import TitleMixin
@@ -11,7 +13,20 @@ class OrderCreateView(TitleMixin, CreateView):
     success_url = reverse_lazy('orders:order_create')
     title = 'UPGrade PC - Оформление заказа'
 
+    def post(self, request, *args, **kwargs):
+        super(OrderCreateView, self).post(request, *args, **kwargs)
+
+        return HttpResponseRedirect(reverse_lazy('orders:order_canceled'))
+
     def form_valid(self, form):
         form.instance.initiator = self.request.user
         return super(OrderCreateView, self).form_valid(form)
 
+
+class CanceledTemplateView(TemplateView):
+    template_name = 'orders/canceled.html'
+
+
+class SuccessTemplateView(TitleMixin, TemplateView):
+    template_name = 'orders/success.html'
+    title = 'UPGrade PC - Спасибо за заказ!'
