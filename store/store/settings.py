80b2e8c24259b8d7f0420996155b8b115789cc1e
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2023-12-21 15:18:14 +0300

    copying the project to the server and performing migrations: install FileZilla FTP Client on our machine copy files: common, orders, products, static, store, users, .env, .flake8, manage.py, requirements.txt
    some changes in settings and requirements.txt
    python manage.py migrate

--- store/store/settings.py
+++ store/store/settings.py
@@ -148,11 +148,11 @@ CACHES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'store_db',
-        'USER': 'postgres',
-        'PASSWORD': 'eremik',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
     }
 }
 
