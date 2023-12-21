1ed7cdfdff2c26c4550d74b2fe2c714e2e2e6c1d
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2023-12-21 10:36:16 +0300

    add ProductDetailView for any product view

--- store/products/urls.py
+++ store/products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from products.views import ProductsListView, basket_add, basket_remove
+from products.views import ProductsListView, basket_add, basket_remove, ProductDetailView
 
 app_name = 'products'
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', ProductsListView.as_view(), name='index'),
     path('category/<int:category_id>/', ProductsListView.as_view(), name='category'),
     path('page/<int:page>/', ProductsListView.as_view(), name='paginator'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 
--- store/products/views.py
+++ store/products/views.py
@@ -2,10 +2,12 @@ from django.contrib.auth.decorators import login_required
 from django.core.cache import cache
 from django.shortcuts import HttpResponseRedirect
 from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from common.views import TitleMixin
 from products.models import Basket, Product, ProductCategory
+
 
 # Create your views here.
 
@@ -32,13 +34,25 @@ class ProductsListView(TitleMixin, ListView):
         return queryset.filter(category__id=category_id) if category_id else queryset
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(ProductsListView, self).get_context_data()
+        context = super(ProductsListView, self).get_context_data(**kwargs)
         categories = cache.get('categories')
         if not categories:
             context['categories'] = ProductCategory.objects.all()
             cache.set('categories', context['categories'], 30)
         else:
             context['categories'] = categories
+        return context
+
+
+class ProductDetailView(DetailView):
+    template_name = 'products/product.html'
+    model = Product
+    context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(**kwargs)
+        context['title'] = f'UPGrade PC - {self.object.name}'
+        context['categories'] = ProductCategory.objects.all()
         return context
 
 
