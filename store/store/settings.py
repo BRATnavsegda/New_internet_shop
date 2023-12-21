03f67f189920ddc8acdcd2ff10af917d033e58af
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2023-12-21 14:18:26 +0300

    server preparation for working with databases: sudo apt update
    sudo apt install postgresql postgresql-contrib
    
    sudo -u postgres psql
    	CREATE DATABASE db_production;
    	CREATE ROLE username_production with password 'password_production';
    	ALTER ROLE "username_production" WITH LOGIN;
    	GRANT ALL PRIVILEGES ON DATABASE "db_production" to username_production;
    	ALTER USER username_production CREATEDB;
    	\q

--- store/store/settings.py
+++ store/store/settings.py
@@ -138,6 +138,7 @@ CACHES = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
