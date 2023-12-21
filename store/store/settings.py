520925a3c7ba12bb7c5c2e885175790952048beb
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2023-12-21 14:29:09 +0300

    virtual environment creation: mkdir store_server cd store_server sudo apt install python3-venv python3 -m venv venv source venv/bin/activate

--- store/store/settings.py
+++ store/store/settings.py
@@ -138,7 +138,6 @@ CACHES = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
